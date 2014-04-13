db0c83b64fa0e5f7b5b0ef1799044d6fbc172310
Jonas Hietala <tradet.h@gmail.com>
2014-04-13 22:05:31 +0200

    Cycle finding.

---
+++ cyclefinding.hxx
@@ -0,0 +1,30 @@
+
+// Return <start, length> of cycle defined by f and x0.
+// Define a function f before.
+template<typename T>
+pair<T, T> cycle_finding(T x0) {
+    T tortoise = f(x0), hare = f(f(x0));
+    // 1st phase, hare 2x speed.
+    while (tortoise != hare) {
+        tortoise = f(tortoise);
+        hare = f(f(hare));
+    }
+
+    // 2nd phase, find start of cycle.
+    T start = 0; hare = x0;
+    while (tortoise != hare) {
+        tortoise = f(tortoise);
+        hare = f(hare);
+        ++start;
+    }
+
+    // 3rd phase, find cycle length. Hare moves, tortoise still.
+    T len = 1; hare = f(tortoise);
+    while (tortoise != hare) {
+        hare = f(hare);
+        ++len;
+    }
+
+    return make_pair(start, len);
+}
+
