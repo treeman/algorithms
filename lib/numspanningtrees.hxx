39c3022e0e0d15539aaddf51dbf0c83d03b49d1d
Jonas Hietala <tradet.h@gmail.com>
2014-05-22 22:45:14 +0200

    Insert conversions to algorithm itsel.

--- lib/numspanningtrees.hxx
+++ lib/numspanningtrees.hxx
@@ -1,8 +1,7 @@
 #include "matrix.hxx"
 
 // Kirchoff's theorem.
-template<typename T>
-T num_spanning_trees(const vector<vi> &adj_list) {
+ll num_spanning_trees(const vector<vi> &adj_list) {
     const int n = adj_list.size();
 
     // 1.
@@ -10,19 +9,18 @@ T num_spanning_trees(const vector<vi> &adj_list) {
     // a[i][i] = degree(i)
     // a[i][j] = -1 if adj(i, j)
     // a[i][j] = 0 otherwise
-    Matrix<T> a(n, n, T(0));
+    Matrix<long double> a(n, n, 0);
     for (int u = 0; u < n; ++u) {
         for (int v : adj_list[u]) {
-            a(u, v) = T(-1);
+            a(u, v) = -1;
         }
         a(u, u) = adj_list[u].size();
     }
 
     // 2. delete any row and any column
-    Matrix<T> q = a.shrink(n - 1, n - 1);
+    Matrix<long double> q = a.shrink(n - 1, n - 1);
 
     // 3. Determinant is result
-    return det(q);
+    return llroundl(det(q));
 }
-
 
